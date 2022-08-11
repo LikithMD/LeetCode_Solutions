ef1c3c863bcdd45f925b6cb9b5d3e52e0a3eea1b
LikithMD <84455660+LikithMD@users.noreply.github.com>
2022-08-11 20:56:39 +0530

    Time: 2 ms (82.18%), Space: 49.6 MB (43.13%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int res=0;
+        int l=0,r=0;
+        while(r<nums.length-1) {
+            int far=0;
+            for(int i=l;i<r+1;i++) {
+                far=Math.max(far,i+nums[i]);
+            }
+            l=r+1;
+            r=far;
+            res++;
+        }
+        return res;
+    }
+}
