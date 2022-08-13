704a91346dae27236dd508fca321fdf22ea05ec0
LikithMD <84455660+LikithMD@users.noreply.github.com>
2022-08-13 12:40:05 +0530

    Time: 33 ms (45.03%), Space: 52.9 MB (37.73%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    int mod=1000000007;
+    public int countVowelPermutation(int n) {
+        long dp[][]=new long[n][5];
+        for(int i=0;i<5;i++)
+            dp[0][i]=1;
+        for(int i=1;i<n;i++) {
+            dp[i][0]=(dp[i-1][1]+dp[i-1][2]+dp[i-1][4])%mod;
+            dp[i][1]=(dp[i-1][0]+dp[i-1][2])%mod;
+            dp[i][2]=(dp[i-1][1]+dp[i-1][3])%mod;
+            dp[i][3]=(dp[i-1][2])%mod;
+            dp[i][4]=(dp[i-1][2]+dp[i-1][3])%mod;
+        }
+        long count=0;
+        for(long x:dp[n-1]) {
+            count+=x;
+            count%=mod;
+        }
+        return (int)count;
+    }
+}
