bda93cf69fc0f58b7693a1ad3dd6da067f3e6d47
LikithMD <84455660+LikithMD@users.noreply.github.com>
2022-08-22 18:31:12 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (58.81%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1) {
-            return true;
-        }
-        if(n%4==0 && n>0) {
-            return isPowerOfFour(n/4);
-        }
-        else 
+        if(n==0)
             return false;
+        if(n==1)
+            return true;
+        if(n%4!=0)
+            return false;
+        return isPowerOfFour(n/4);
     }
 }
